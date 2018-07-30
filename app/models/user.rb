50a09c32db21dfa109f32c4798f75b70a641d96e
Cong <thanhcong.uet97@gmail.com>
2018-07-30 08:40:21 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-	has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: Settings.maximum1}
+  validates :email, presence: true, length: {maximum: Settings.maximum2},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.minimum}
+  has_secure_password
 end
